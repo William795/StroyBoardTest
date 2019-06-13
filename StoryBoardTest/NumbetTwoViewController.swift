236c819e945e7032669453803ec98f2ac56b18d6
William Moody <williammoody@Williams-MacBook-Air.local>
2019-06-13 15:41:10 -0600

    adding storyboard

---
+++ StoryBoardTest/NumbetTwoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NumbetTwoViewController.swift
+//  StoryBoardTest
+//
+//  Created by William Moody on 6/13/19.
+//  Copyright © 2019 William Moody. All rights reserved.
+//
+
+import UIKit
+
+class NumbetTwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
